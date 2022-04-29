d25b9ff439e5a19c9b8a84ecb01198c9c4223eec
Priyanka <priyankahansi95@gmail.com>
2022-04-29 14:46:32 +0530

    added:calculated cartesian system

--- src/com/bridgelabz/LineComparison.java
+++ src/com/bridgelabz/LineComparison.java
@@ -1,7 +1,15 @@
 package com.bridgelabz;
 
 public class LineComparison {
+    void method1() {
+        int x1 = 1, y1 = 1, x2 = 4, y2 = 4;
+        double lengthOfFirstPoints;
+        lengthOfFirstPoints = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+        System.out.println("length of line==> " + lengthOfFirstPoints);
+    }
+
     public static void main(String[] args) {
-        System.out.println("welcom to Linecomparison");
+        LineComparison obj1 = new LineComparison();
+        obj1.method1();
     }
 }
